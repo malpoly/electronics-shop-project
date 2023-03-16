4f635f5009155c20229bc355f2a50aa39f4199ba
malpoly <marry2691@yandex.ru>
2023-03-16 21:23:24 +0300

    homework-2 изменила файл item.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) < 10:
+            self.__name = value
+        else:
+            print("Exception: Длина наименования товара превышает 10 символов.")
 
 
     def calculate_total_price(self) -> float:
@@ -33,4 +46,19 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = int(self.pay_rate * self.price)
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv"""
+        Item.all = [] #Список обнулила, чтобы при проверке работало условие по длине списка = 5, иначе оставался item = Item('Телефон', 10000, 5) в списке
+        with open('C:\skypro\lesson_code\lesson_13.1\src\items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
+
+
+    @staticmethod
+    def string_to_number(number) -> int:
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(number))
 
