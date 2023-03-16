e6c3926e9c9ecd4733d928dec0da3139819701f3
malpoly <marry2691@yandex.ru>
2023-03-16 21:44:55 +0300

    homework-2 изменила test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -10,8 +10,19 @@ def test_item_init(item1):
     assert item1.quantity == 20
 
 def test_item_calculate_total_price(item1):
-    assert 200000
+    assert item1.calculate_total_price() == 200000
 
 def test_item_apply_discount(item1):
     Item.pay_rate = 0.8
-    assert 8000
+    item1.apply_discount()
+    assert item1.price == 8000
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) != 0
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
